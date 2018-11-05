50f2d87322fd584ad59f304c44a0b8374fff00b6
Naomi Lui-HIng <naomiluihing@cmail.carleton.ca>
2018-11-05 09:09:29 -0500

    Minor changes to preliminary code

--- UltrasonicSeonsor/UltrasonicSeonsor.ino
+++ UltrasonicSeonsor/UltrasonicSeonsor.ino
@@ -1,22 +1,25 @@
-#incluse <HCSR04.h>
+#include <HCSR04.h>
 
 //Initialize Ultrasonic Sensor and the pins it uses
 int trigPin = 11; //trigger pin connected to pin 11
 int echoPin = 12; //echo Pin connected to pin 12
-int freq = 500; //delay frequency of ultrasonic sensor readings in miliseconds
+int freq = 500; //delay frequency of ultrasonic sensor readings in milliseconds
 UltraSonicDistanceSensor distanceSensor(trigPin, echoPin);
+
+bool car; //state variable if car is at parking lot entrance or not
+
 
 void setup() 
 {
     //Initialize serial connection
     Serial.begin(9600);
-
-  
 }
 
 void loop() 
 {
     double distance = distanceSensor.measureDistanceCm();
-    Serial.println(distance);
-    delay(freq); //using predetermined time, in miliseconds, delay after each measurment and print
+    
+    car = isCar(d1,d2); //test if car is there or not
+    delay(freq); //using predetermined time, in milliseconds, delay after each measurement and return
+    
 }
